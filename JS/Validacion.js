f997da0fb47bf9be09b87ea880600214c344c7b0
Maria <mariabg27200@gmail.com>
2023-09-28 10:18:33 -0500

    agregando validación

---
+++ JS/Validacion.js
@@ -0,0 +1,10 @@
+const inputNacimiento = document.querySelector('#birth');
+
+inputNacimiento.addEventListener("blur", (evento) => {
+    console.log(evento.target)
+});
+
+function validarNacimiento(inputNacimiento){
+    const fecha = input.value;
+    console.log(fecha);
+}
